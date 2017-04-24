138ea6e33937a0b2d4071270a61458567753e191
Dong Pei <dpei@BSM-FARNOOSH.local>
2017-04-24 16:49:44 -0500

    added method for canvas color

---
+++ src/GUI_module/MyDisplay.java
@@ -0,0 +1,13 @@
+package GUI_module;
+
+import processing.core.PApplet;
+
+public class MyDisplay extends PApplet {
+	public void setup(){
+		size(400, 400);
+		background(0,0,0);
+	}
+	public void draw(){
+		
+	}
+}
