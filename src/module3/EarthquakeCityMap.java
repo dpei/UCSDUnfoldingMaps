fc9c3bb83f66705027ba102c4fcf805de01ef91e
Dong Pei <peidong2009@gmail.com>
2017-04-25 16:50:50 -0500

    added dots to map

--- src/module3/EarthquakeCityMap.java
+++ src/module3/EarthquakeCityMap.java
@@ -16,6 +16,7 @@ import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import de.fhpotsdam.unfolding.providers.Google;
 import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
+import de.fhpotsdam.unfolding.providers.Microsoft;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 
 //Parsing library
@@ -24,8 +25,9 @@ import parsing.ParseFeed;
 /** EarthquakeCityMap
  * An application with an interactive map displaying earthquake data.
  * Author: UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
+ * author modify Dong Pei
  * Date: July 17, 2015
+ * last modified: Apr. 25. 2017
  * */
 public class EarthquakeCityMap extends PApplet {
 
@@ -33,7 +35,7 @@ public class EarthquakeCityMap extends PApplet {
 	private static final long serialVersionUID = 1L;
 
 	// IF YOU ARE WORKING OFFLINE, change the value of this variable to true
-	private static final boolean offline = false;
+	private static final boolean offline = true;
 	
 	// Less than this threshold is a light earthquake
 	public static final float THRESHOLD_MODERATE = 5;
@@ -46,10 +48,16 @@ public class EarthquakeCityMap extends PApplet {
 	// The map
 	private UnfoldingMap map;
 	
-	//feed with magnitude 2.5+ Earthquakes
-	private String earthquakesURL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
-
+	SimplePointMarker earthquakeMarker;
 	
+	//feed with magnitude 2.5+ Earthquakes
+	// TODO import error "White spaces are required between publicId and systemId"
+
+
+	private String earthquakesURL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
+	
+	//
+		
 	public void setup() {
 		size(950, 600, OPENGL);
 
@@ -60,7 +68,7 @@ public class EarthquakeCityMap extends PApplet {
 		else {
 			map = new UnfoldingMap(this, 200, 50, 700, 500, new Google.GoogleMapProvider());
 			// IF YOU WANT TO TEST WITH A LOCAL FILE, uncomment the next line
-			//earthquakesURL = "2.5_week.atom";
+			earthquakesURL = "2.5_week.atom";
 		}
 		
 	    map.zoomToLevel(2);
@@ -68,10 +76,18 @@ public class EarthquakeCityMap extends PApplet {
 			
 	    // The List you will populate with new SimplePointMarkers
 	    List<Marker> markers = new ArrayList<Marker>();
-
+	    
 	    //Use provided parser to collect properties for each earthquake
 	    //PointFeatures have a getLocation method
 	    List<PointFeature> earthquakes = ParseFeed.parseEarthquake(this, earthquakesURL);
+	    
+	    // Loop through the PointFeature objects and add each point into map.
+	    for (int i = 0; i< earthquakes.size(); i++){
+	    	map.addMarker(createMarker(earthquakes.get(i)));
+	    }
+	    
+	    
+	    
 	    
 	    // These print statements show you (1) all of the relevant properties 
 	    // in the features, and (2) how to get one property and use it
@@ -101,7 +117,7 @@ public class EarthquakeCityMap extends PApplet {
 	
 	public void draw() {
 	    background(10);
-	    map.draw();
+	    map.draw();	    
 	    addKey();
 	}
 
